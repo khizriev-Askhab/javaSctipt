746d0209fe354eb06a7b9570a65872f85f4a07d5
khizriev <askhab.askhabb.khizriev@mail.ru>
2022-10-26 13:29:12 +0300

    script

---
+++ document.js
@@ -0,0 +1,43 @@
+                // ЗАДАЧ-1 
+let now = new Date
+console.log (now)
+
+                /* ЗАДАЧА-2
+
+- Какой тип данных нужен для сохранения числа ПИ 3.14?
+
+ОТВЕТ:   NUMBER */
+
+                //ЗАДАЧА-3
+
+const admin = "askhab";
+admin = "djon";
+
+
+
+
+
+
+                //ЗАДАЧА-4
+
+let name = prompt ("Askhab") ;
+let address = prompt ("Chechny");
+let phone = prompt (89390000000);
+let presentation =`Студент ${name}  адрес ${address}  номер телефона ${phone}`;
+console.log( presentation);
+                
+
+
+
+
+
+
+
+
+              
+                //ЗАДАЧА-5
+
+let rubles = 1;
+let dollars =61;
+let count = rubles + dollars
+console.log(count);
